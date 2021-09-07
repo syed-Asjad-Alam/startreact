429c70613c5d436225f5414ae6bb21da769987ee
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-09-07 16:08:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>
-          updated by Asjad
-          </br>
+          <br>Updated by Asjad Alam</br>
         </p>
         <a
           className="App-link"
