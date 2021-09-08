307e87b3fa90651f50f24635894f0e4a34e25558
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-09-08 23:15:08 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    Hello World by Asjad
+  </div>,
   document.getElementById('root')
 );
 
